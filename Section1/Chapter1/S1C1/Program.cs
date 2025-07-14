6aa28e8bca240eef026cf71f92c066a0579d01b8
a.konkin <temakonkin@gmail.com>
2025-07-14 21:57:32 +0600

    - Add S1C1_P1_L8

--- Section1/Chapter1/S1C1/Program.cs
+++ Section1/Chapter1/S1C1/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using S1C1;
+Console.WriteLine("Hello, World!");
+
+var p1 = new S1C1_P1();
+p1.S1C1_P1_L8();
--- Section1/Chapter1/S1C1/Src/S1C1_P1.cs
+++ Section1/Chapter1/S1C1/Src/S1C1_P1.cs
@@ -17,4 +17,47 @@ public class S1C1_P1
     {
         Console.WriteLine("[3] Докажите, что значение a*b может быть меньшим, чем значение min(a,b).");
     }
+
+    /// <summary>
+    /// [3] Докажите правильность следующего рекурсивного алгоритма умножения двух натуральных чисел для всех целочисленных констант c >= 2:
+    /// Multiply(y, z)
+    ///    if z=0 then return (0) else
+    /// return (Multiply(cy, floor(z/c)) + y*(z mod c))
+    /// </summary>
+    public void S1C1_P1_L8()
+    {
+        Console.WriteLine("[3] Докажите правильность следующего рекурсивного алгоритма умножения двух натуральных чисел для всех целочисленных констант c >= 2:");
+
+        var c = 2;
+
+        int Multiply(int y, int z)
+        {
+            if (z == 0)
+            {
+                Console.WriteLine($"z = {z}, y = {y}, c = {c}");
+
+                return 0;
+            }
+
+            Console.WriteLine($"z = {z}, y = {y}, c = {c}");
+
+            return Multiply(c * y, z / c) + y * (z % c);
+        }
+
+        for (c = 2; c < 4; c++)
+        {
+            for (var i = 2; i < 4; i++)
+            {
+                for (var j = 2; j < 4; j++)
+                {
+                    Console.WriteLine($"Multiply({i}, {j}) = {Multiply(i, j)} = {i * j}");
+
+                    if (Multiply(i, j) != i * j)
+                    {
+                        throw new Exception();
+                    }
+                }
+            }
+        }
+    }
 }
